2b08dd942746190f6900edfcfd87ef810b1342ee
RenamerXDS <147261601+RenamerXDS@users.noreply.github.com>
2023-10-07 23:08:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "c5ec8c471ec25acc97db0f5089c7a63a")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6471049323:AAGobY4nuRxiLPodISWi3sqCSfPstjag93g") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "Arsenal_Bots_Updates) 
+FORCE_SUB = os.environ.get("FORCE_SUB", "Arsenal_Bots_Updates") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
